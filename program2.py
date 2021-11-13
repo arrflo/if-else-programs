fad266c4f043e042b58e05c03fea3795f17e1bb0
romalynfloranda <romalynfloranda0203@gmail.com>
2021-11-14 01:59:53 +0800

    first step done, program asks for 3 numbers

---
+++ program2.py
@@ -0,0 +1,14 @@
+
+
+
+#create a program that asks 3 numbers
+def getNumber ():
+    _first = int(input("First Number:"))
+    _second = int(input("Second Number:"))
+    _third = int(input("Third Number:"))
+    return _first, _second, _third
+
+first, second, third = getNumber()
+
+#find the lowest number using only if-else statement
+#display the lowest number
