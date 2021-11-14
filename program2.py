0726431c02c3d0014b98dcca47370e6b61d10ffd
romalynfloranda <romalynfloranda0203@gmail.com>
2021-11-14 11:23:17 +0800

    if-else statements were added, first try

--- program2.py
+++ program2.py
@@ -11,4 +11,32 @@ def getNumber ():
 first, second, third = getNumber()
 
 #find the lowest number using only if-else statement
-#display the lowest number
+if first == second == third:    #equal lahat
+    print ("All numbers are equal")
+else: 
+    if first == second and first < third:   #equal yung first at second
+        print(f"{first} is the smallest number.")
+    else:
+        if first == second and first > third:
+            print (f"{third} is the smallest number.")
+        else:
+            if second == third and second < first:     #equal yung second at third
+                  print(f"{second} is the smallest number.")
+            else:
+                if second == third and second > first:
+                    print (f"{first} is the smallest number.")
+                else:
+                    if first == third and first < second:   #equal yung first at third
+                        print (f"{first} is the smallest number.")
+                    else:
+                        if first == third and first > second:
+                            print(f"{second} is the smallest number.")
+                        else:
+                            if first < second and first < third:    #first ang smallest
+                                print(f"{first} is the smallest number.")
+                            else:
+                                if second < first and second < third:   #second ang smallest
+                                    print (f"{second} is the smallest number.")
+                                else:
+                                    if third < first and third < second:    #third and smallest
+                                        print(f"{third} is the smallest number.")   #display the lowest number
